5a765efea1de16987d6038458a40f4f7049f5993
Nadno <eunadno@gmail.com>
2022-04-04 21:12:43 -0300

    improvement(myQuery): use getElementById for queries starting with '#'

--- src/app.js
+++ src/app.js
@@ -9,13 +9,27 @@ const myQuery = (() => {
     if (element == null) return;
     if (typeof element == 'string') element = document.querySelector(element);
 
-    if (typeof element == 'string') element = document.querySelector(element);
+  function myQuery(element) {
+    if (element == null) return null;
+    if (typeof element == 'string') element = getElement(element);
     if (!(element instanceof Element)) return null;
 
-    return Object.assign(Object.create(proto), {
-      element,
+    return Object.create(proto, {
+      element: { value: element },
     });
-  };
+  }
+
+  function getElement(element) {
+    const isId = element.startsWith('#');
+    if (isId) {
+      const id = element.replace(/^#/, '');
+      return document.getElementById(id);
+    }
+
+    return document.querySelector(element);
+  }
+
+  return myQuery;
 })();
 
 window.myQuery = window.$ = myQuery;
