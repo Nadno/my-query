ede07d2843dc0de319be4d06e32f32f4e19cca96
Nadno <eunadno@gmail.com>
2023-01-29 12:18:12 -0300

    refactor(main): extract interface MyQuery from myQuery function

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
-import MyQuery from './modules/my-query';
+import MyQueryClass from './modules/my-query';
+
 import getElement from './utils/getElement';
 
-function myQuery<T extends Element>(query: string): MyQuery<T> | null;
-function myQuery<T extends Element>(element: T): MyQuery<T>;
-function myQuery(queryOrElement: any): any {
+declare interface MyQuery {
+  <T extends Element>(query: string): MyQueryClass<T> | null;
+  <T extends Element>(element: T): MyQueryClass<T>;
+}
+
+const myQuery = function myQuery(queryOrElement: unknown): unknown {
   const element =
     typeof queryOrElement === 'string'
       ? getElement(queryOrElement)
       : (queryOrElement as Element);
 
   if (element == null) return null;
-  return new MyQuery(element);
-}
+  return new MyQueryClass(element);
+} as MyQuery;
 
 export default myQuery;
