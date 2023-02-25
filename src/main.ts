0d34796587d729347ce9d0fd93deacf437ab489a
Nadno <eunadno@gmail.com>
2023-02-25 13:17:56 -0300

    feat(my-query): assign my-query utilities to myQuery factory

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import MyQueryClass from './modules/my-query';
 
 import getElement from './utils/getElement';
+import * as myQueryUtils from './utils/my-query';
+import type { MyQueryUtils } from './utils/my-query';
 
-declare interface MyQuery {
+declare interface MyQuery extends MyQueryUtils {
   <T extends Element>(query: string): MyQueryClass<T>;
   <T extends Element>(element: T): MyQueryClass<T>;
 }
@@ -16,6 +18,8 @@ const myQuery = function myQuery(queryOrElement: unknown): unknown {
   if (element == null) return null;
   return new MyQueryClass(element);
 } as MyQuery;
+
+Object.assign(myQuery, myQueryUtils);
 
 export { MyQueryClass as MyQuery };
 export default myQuery;
